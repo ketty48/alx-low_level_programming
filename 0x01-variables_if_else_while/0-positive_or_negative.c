72b874ec8e286afd0ee0425424a6cf721d0eb27e
magnifique <ketymagnifique@gmail.com>
2023-04-23 05:34:50 -0700

    my commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,17 +17,17 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX /2;
 
-if(n > 0)
+if (n > 0)
 {
-printf("%d is Positive\n", n);
+printf("%i is Positive\n", n);
 }
-else if(n == 0)
+else if (n == 0)
 {
-printf("%d is Zero\n", n);
+printf("%i is Zero\n", n);
 }
 else
 {
-printf("%d is Negative\n", n);
+printf("%i is Negative\n", n);
 }
 return (0);
 }
